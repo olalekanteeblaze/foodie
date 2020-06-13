d774a89656b66812bc184d622c49929b5f2d4b68
olalekanteeblaze <olalekanteeblaze@gmail.com>
2020-06-13 04:11:50 +0100

    quick reply

--- sendAPI.js
+++ sendAPI.js
@@ -11,13 +11,13 @@ module.exports = function callSendAPI(senderPSID, response) {
     
       // Send the HTTP request to the Messenger Platform
       request({
-        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "uri": "https://graph.facebook.com/v7.0/me/messages",
         "qs": { "access_token": process.env.ACCESS_TOKEN },
         "method": "POST",
         "json": request_body
       }, (err, res, body) => {
         if (!err) {
-          console.log('message sent!')
+          console.log('message sent!', res)
         } else {
           console.error("Unable to send message:" + err);
         }
