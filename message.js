e944ef72b713a488cc891086b151b87cd6ee036f
olalekanteeblaze <olalekanteeblaze@gmail.com>
2020-06-13 02:04:42 +0100

    prevent some messages

--- message.js
+++ message.js
@@ -7,57 +7,63 @@ module.exports = function handleMessage(senderPSID, receivedMessage) {
     let processing = {
         text: "Preparing your recipe..."
     }
-    callSendAPI(senderPSID, processing)
-    request({
-        "uri": 'https://api.spoonacular.com/recipes/search',
-        "qs": {
-            "query": message,
-            "number": 1,
-            "apiKey": process.env.API_KEY,
-            "instructionsRequired": true
-        },
-        "method": "get"
-    }, (err, res, body) => {
-        if(!err){
-            let id = JSON.parse(res.body).results[0].id
-            request({
-                "uri": `https://api.spoonacular.com/recipes/${id}/information`,
-                "qs": {
-                    "apiKey": process.env.API_KEY
-                },
-                "method": "get"
-            },(err, res, body) => {
-                if(!err) { 
-                    let extendedIngredients = 'These are the needed ingredients'
-                    const ingredients = JSON.parse(res.body).extendedIngredients
-                    console.log(ingredients)
-                    ingredients.forEach((ingredient) => {
-                        extendedIngredients += `${ingredient.originalName}`
-                    })
-                    ingredientText = {
-                        text: extendedIngredients
-                    }
-                    callSendAPI(senderPSID, ingredientText)
-                    response = {
-                        text: JSON.parse(res.body).instructions,
-                    }
-                    const image = {
-                        attachment: {
-                            type: "image",
-                            payload: {
-                                url: JSON.parse(res.body).image,
-                                is_reusable: true
+    if(message != 'Enter name of food' || 'Take a picture or select from gallery') {
+        callSendAPI(senderPSID, processing)
+        request({
+            "uri": 'https://api.spoonacular.com/recipes/search',
+            "qs": {
+                "query": message,
+                "number": 1,
+                "apiKey": process.env.API_KEY,
+                "instructionsRequired": true
+            },
+            "method": "get"
+        }, (err, res, body) => {
+            if(!err){
+                let id = JSON.parse(res.body).results[0].id
+                request({
+                    "uri": `https://api.spoonacular.com/recipes/${id}/information`,
+                    "qs": {
+                        "apiKey": process.env.API_KEY
+                    },
+                    "method": "get"
+                },(err, res, body) => {
+                    if(!err) { 
+                        let extendedIngredients = 'These are the needed ingredients'
+                        const ingredients = JSON.parse(res.body).extendedIngredients
+                        console.log(ingredients)
+                        ingredients.forEach((ingredient) => {
+                            extendedIngredients += `${ingredient.originalName}`
+                        })
+                        ingredientText = {
+                            text: extendedIngredients
+                        }
+                        callSendAPI(senderPSID, ingredientText)
+                        response = {
+                            text: JSON.parse(res.body).instructions,
+                        }
+                        const image = {
+                            attachment: {
+                                type: "image",
+                                payload: {
+                                    url: JSON.parse(res.body).image,
+                                    is_reusable: true
+                                }
                             }
                         }
-                    }
-                    callSendAPI(senderPSID, response)
-                    callSendAPI(senderPSID, image)
-                    } else {
-                        console.error(err)
-                    }
-            })
-        } else {
-            console.error(err)
-        }
-    })
+                        const greeting = {
+                            text: "Enjoy your meal, hope to see you again"
+                        }
+                        callSendAPI(senderPSID, response)
+                        callSendAPI(senderPSID, image)
+                        callSendAPI(senderPSID, greeting)
+                        } else {
+                            console.error(err)
+                        }
+                })
+            } else {
+                console.error(err)
+            }
+        })
+    }
 }
