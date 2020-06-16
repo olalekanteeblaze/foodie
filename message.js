74b0947b67536fc2331c0fa3a041c09814ab0a85
olalekanteeblaze <olalekanteeblaze@gmail.com>
2020-06-16 19:20:33 +0100

    show classify response

--- message.js
+++ message.js
@@ -114,7 +114,70 @@ module.exports = function handleMessage(senderPSID, receivedMessage) {
                 "method": "get"
             }, (err, res, body) => {
                 if(!err) {
-                    console.log(res)
+                    const category = res.body.category.name
+                    const prob = parseInt(res.body.category.probability)
+                    const message = {
+                        text: `I'm ${prob}% sure this food belong to the ${category} category. Preparing your recipe....`
+                    } 
+                    callSendAPI(senderPSID, message)
+                    request({
+                        "uri": 'https://api.spoonacular.com/recipes/search',
+                        "qs": {
+                            "query": category,
+                            "number": 1,
+                            "apiKey": process.env.API_KEY,
+                            "instructionsRequired": true
+                        },
+                        "method": "get"
+                    }, (err, res, body) => {
+                        if(!err){
+                            let id = JSON.parse(res.body).results[0].id
+                            request({
+                                "uri": `https://api.spoonacular.com/recipes/${id}/information`,
+                                "qs": {
+                                    "apiKey": process.env.API_KEY
+                                },
+                                "method": "get"
+                            },(err, res, body) => {
+                                if(!err) { 
+                                    let extendedIngredients = 'These are the needed ingredients'
+                                    const ingredients = JSON.parse(res.body).extendedIngredients
+                                    ingredients.forEach((ingredient) => {
+                                        extendedIngredients += `${ingredient.original}
+                                        `
+                                    })
+                                    const ingredientText = {
+                                        text: extendedIngredients
+                                    }
+                                    callSendAPI(senderPSID, ingredientText)
+                                    response = {
+                                        text: JSON.parse(res.body).instructions,
+                                    }
+                                    const image = {
+                                        attachment: {
+                                            type: "image",
+                                            payload: {
+                                                url: JSON.parse(res.body).image,
+                                                is_reusable: true
+                                            }
+                                        }
+                                    }
+                                    const greeting = {
+                                        text: "Enjoy your meal, hope to see you again"
+                                    }
+                                    callSendAPI(senderPSID, response)
+                                    callSendAPI(senderPSID, image)
+                                    setTimeout( () => callSendAPI(senderPSID, greeting), 3000)
+                                    } else {
+                                        console.error("err1")
+                                    }
+                            })
+                        } else {
+                            console.error("err2")
+                        }
+                        setTimeout(() => callSendAPI(senderPSID, quickReply), 10000)
+                    })
+                    
                 } else {
                     console.log("error occured parsing image")
                 }
